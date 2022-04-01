b6ee2a51de9ee10ec9928bd8f2bb25f040f3be5a
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2022-04-01 13:23:46 +0530

    fix: fixed double import link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { Header, Footer } from './components'
 import { Router } from './router/Router'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
